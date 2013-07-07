523cfff4c545ef81c02b56b81c4096d7f19c5503
Matt Ma <mma@adchemy.com>
2013-07-07 08:15:29 -0700

    remove the canvas object, make all of methods is available to container, then container could return canvas objects

--- src/common/container.js
+++ src/common/container.js
@@ -2,85 +2,92 @@
 d3.ma = {};
 
 /*
-	return element: All methods return svg canvas object for easy chaining
-	E.G: d3.ma.canvas('body').margin({top: 80, left: 80}).box(600, 500)
-			.select('.canvas'))  // always need to select g element/.canvas element to append children
+	return element: Most of methods return svg canvas object for easy chaining
+	E.G: d3.ma.container('body').margin({top: 80, left: 80}).box(600, 500)
 
-	d3.ma.canvas(selection) is absolutely required, others are optional.
+		var container = d3.ma.container('#vis'),
+			canvas = container.canvas().chart("LabeledCirclesChart");
 
-	canvas() will take a css selector, which will be the container to contain the svg element
+	d3.ma.container(selector) is absolutely required, others are optional.
+
+	container() will take a css selector, which will be the container to contain the svg element
 
 	margin()  is optional, to set/get margin object, must take an object as value, e.g. {top: 80}, normally come before box()
 
 	box() is optional, by default, it will take the container width and height. to set/get/retrieve the svg/container width and height, if  only one value provide, width and height would be the same
 
-	width() and height() is optional, will set/get the canvas width and height
+	canvasW() and canvasH() is optional, will set/get the canvas width and height. Normally, do not need to set explicitly, Use box() will auto set the canvas width and height as properly
  */
 
-d3.ma.container = function(selection) {
+d3.ma.container = function(selector) {
 
-	var container = d3.select(selection);
+	var selection = d3.select(selector);
 
 	var margin = {top: 30, right: 20, bottom: 20, left: 30},
-		containerW = container[0][0].clientWidth || document.body.clientWidth || 960,
-		containerH = container[0][0].clientHeight || document.body.clientHeight || 540,
-		width,
-		height,
-		canvas = container.append('svg'),  // append svg element to the selection
+		containerW = selection[0][0].clientWidth || document.body.clientWidth || 960,
+		containerH = selection[0][0].clientHeight || document.body.clientHeight || 540,
+		canvasW,
+		canvasH,
+		container = selection.append('svg'),  // Create container, append svg element to the selection
 		// Create a new group element append to svg
 		// Set the canvas layout with a good amount of offset, has canvas class for easy targeting
-		g = canvas.append('g').classed('canvas', true);
+		g = container.append('g').classed('canvas', true);
 
-	canvas.width = function(_width, boxCalled) {
+	container.canvasW = function(_width, boxCalled) {
 		if (!arguments.length) return g.attr("width");
 		if(boxCalled) _width = parseInt(_width) - margin.left - margin.right;
 		g.attr('width', _width);
-		return canvas;
+		return container;
 	};
 
-	canvas.height = function(_height, boxCalled) {
+	container.canvasH = function(_height, boxCalled) {
 		if(!arguments.length) return g.attr("height");
 		if(boxCalled) _height = parseInt(_height) - margin.top - margin.bottom;
 		g.attr('height', _height);
-		return canvas;
+		return container;
 	};
 
-	canvas.box = function(_width, _height) {
+	container.box = function(_width, _height) {
 		if(!arguments.length) {
-			var m = canvas.margin();
+			var m = container.margin();
 			return {
-				'containerWidth': canvas.width() + m.left + m.right,
-				'containerHeight': canvas.height() + m.top + m.bottom
+				'containerWidth': container.canvasW() + m.left + m.right,
+				'containerHeight': container.canvasH() + m.top + m.bottom
 			};
 		}
 
-		// When arguments are more than one, set svg and canvas width & height
+		// When arguments are more than one, set svg and container width & height
 		var h = (_height) ? _height : _width;
 
-		canvas.width(_width, true);
-		canvas.height(h, true);
+		container.canvasW(_width, true);
+		container.canvasH(h, true);
 
-		canvas.attr({
+		container.attr({
 			'width': _width,
 			'height': h
 		});
 
 		gTransform();
 
-		return canvas;
+		return container;
 	};
 
-	canvas.margin = function(_) {
+	container.margin = function(_) {
 		if (!arguments.length) return margin;
 		margin.top  = typeof _.top  != 'undefined' ? _.top  : margin.top;
 		margin.right  = typeof _.right  != 'undefined' ? _.right  : margin.right;
 		margin.bottom = typeof _.bottom != 'undefined' ? _.bottom : margin.bottom;
 		margin.left   = typeof _.left   != 'undefined' ? _.left   : margin.left;
 
-		// After set the new margin, and maintain svg container width and height, canvas width and height ratio
-		canvas.box(containerW, containerH);
+		// After set the new margin, and maintain svg container width and height, container width and height ratio
+		container.box(containerW, containerH);
 
-		return canvas;
+		return container;
+	};
+
+	// Return the canvas object for easy chaining
+	container.canvas = function(){
+		return container.select('g');
 	};
 
 	// Set the canvas transform attr, call it when needed
@@ -89,8 +96,8 @@ d3.ma.container = function(selection) {
 	}
 
 	// Set the svg container width and height
-	// Set the canvas width and height, and its transform values
-	canvas.box(containerW, containerH);
+	// Set the container width and height, and its transform values
+	container.box(containerW, containerH);
 
-	return canvas;
+	return container;
 };
