fceeaed5f132963c74b1cd29fa9f6656f2d64daa
Matt Ma <mma@adchemy.com>
2013-07-18 15:45:44 -0700

    adding the dots to the line chart

---
+++ src/basic/circle.js
@@ -0,0 +1,32 @@
+/*
+
+ */
+d3.chart('Base').extend('Circle', {
+
+	initialize: function(options) {
+		options = options || {};
+
+		var self = this;
+
+		this.line = d3.svg.line();
+
+		this.path = this.base.append('path').attr({
+			'class': 'line',
+		});
+
+		this.line
+			.x(function(d) { return self.xScale(d.x); })
+			.y(function(d) { return self.yScale(d.y);  });
+
+		return this;
+	},
+
+	transform: function(data) {
+
+		if(this.onDataBind) { this.onDataBind(); }
+
+		this.path.datum(data).attr('d', this.line);
+
+		return data;
+	},
+});
