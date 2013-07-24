ebb18ac303d16a48d92dbb6b79699aa2af98fe53
Matt Ma <mma@adchemy.com>
2013-07-24 07:02:19 -0700

    update the legend html example to handle legend info

--- src/components/legend.js
+++ src/components/legend.js
@@ -5,8 +5,9 @@
 d3.chart('Legend', {
 	initialize: function() {
 
-		var dispatch = d3.dispatch('d3maLegendMouseEnter', 'd3maLegendMouseout', 'd3maLegendClick'),
-			color = d3.ma.Color();
+		var color = d3.ma.Color();
+
+		this.dispatch = d3.dispatch('d3maLegendMouseenter', 'd3maLegendMouseout', 'd3maLegendClick');
 
 		this.layer('legend', this.base, {
 			// select the elements we wish to bind to and bind the data to them.
@@ -14,17 +15,6 @@ d3.chart('Legend', {
 				var chart = this.chart();
 
 				if(chart.onDataBind) { chart.onDataBind(chart); }
-
-				this
-					.on('mouseenter', function(d, i){
-						dispatch.d3maLegendMouseEnter(chart, this);
-					})
-					.on('mouseout', function(d, i){
-						dispatch.d3maLegendMouseout(chart, this);
-					})
-					.on('click', function(d, i){
-						dispatch.d3maLegendClick(chart, this);
-					});
 
 				return this.selectAll('.legend').data(data);
 			},
@@ -41,7 +31,22 @@ d3.chart('Legend', {
 				'enter': function() {
 					var chart = this.chart();
 
-					this.classed('disabled', function(d) { return d.disabled });
+					this
+						.on('mouseenter', function(d, i){
+							chart.dispatch.d3maLegendMouseenter(d, i, chart, this);
+						})
+						.on('mouseout', function(d, i){
+							chart.dispatch.d3maLegendMouseout(d, i, chart, this);
+						})
+						.on('click', function(d, i){
+							chart.dispatch.d3maLegendClick(d, i, chart, this);
+						});
+
+					this.classed('disabled', function(d) { return d.disabled }).attr({
+						'transform': function(d, i) {
+							return  'translate( 20, ' + (i * 20 + 10) + ' )'
+						}
+					});
 
 					this.append('circle').attr({
 						'stroke-width': 2,
@@ -53,15 +58,14 @@ d3.chart('Legend', {
 					this.append('text').attr({
 						'text-anchor': 'start',
 						'dy': '.32em',
-						'dx': 8
-					}).text(function(d,i){ return d.legend });
+						'dx': 12
+					}).text(function(d,i){ return d.key });
+
 
 					// onEnter fn will take two args
 					// chart  # refer to this context, used it to access xScale, yScale, width, height, etc. chart property
 					// this   # refer to each individual group just appended by insert command
 					if(chart.onEnter) { chart.onEnter(chart, this); }
-
-					chart._onWindowResize(chart, this);
 				},
 
 				'exit': function() {
