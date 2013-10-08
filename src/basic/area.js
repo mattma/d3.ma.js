1b83aaa1505638a2a819c5f0770b52bbbfa1f833
Matt Ma <mma@adchemy.com>
2013-10-08 10:24:46 -0700

    update the area variable instead of line var

--- src/basic/area.js
+++ src/basic/area.js
@@ -1,41 +1,104 @@
+/*
+	by default, the line is expect the data like this structure { x: 0.3434, y: 0.3242}
+	but if the object data is rather different like { z: 0.3434, y: 0.3242}
 
-d3.chart('Line').extend('Area', {
+
+	var line = this.mixin("Line", this.base.append('g').classed('lines', true), {
+		info: containerInfo
+	});
+
+	line.line.x(function(d){
+		return line.xScale(d.z);
+	});
+
+	Use this.line # this will be replaced with its current context like line here.
+	context.xScale will access the this.xScale here.
+
+	class:  dot
+
+	this.path leave in transform, so that it could be extended by Area chart
+ */
+d3.chart('Base').extend('Area', {
 
 	initialize: function(options) {
 		options = options || {};
 
-		var self = this;
+		this.layer('area', this.base, {
+			dataBind: function(data) {
+				var chart = this.chart();
 
-		this.area = d3.svg.area();
+				chart.area = d3.svg.area();
 
-		this.areaPath = this.base.append('path').attr({
-			'class': 'area',
-		});
+				// Setup the auto resize to handle the on resize event
+				// chart.dispatch.on('d3maSingleWindowResize', function(chart, single){
+				// 	single.attr({ 'd': chart.line });
+				// });
 
-		this.area
-			.x(this.line.x())
-			.y1(this.line.y())
-			.y0(this.yScale(0));
+				chart.area
+					.x(function(d) { return chart.xScale(d.x); })
+					.y(function(d) { return chart.yScale(d.y);  });
 
-		return this;
-	},
+				if(chart.onDataBind) { chart.onDataBind(chart, data,  (options.data) ? options.data : undefined ); }
 
-	transform: function(data) {
+				// data[options.data]  will return a single array, data will bind path element to each array index,
+				// by pushing options array into an anonymous array, ONLY one path element will be created
+				//return this.selectAll('path').data( (options.data) ? [ data[options.data] ]: data );
+				return this.selectAll('path').data( [data] );
+			},
 
-		if(this.onDataBind) { this.onDataBind(); }
+			insert: function(){
+				var chart = this.chart();
+				if(chart.onInsert) { chart.onInsert(chart); }
+				return this.append('path').classed('line', true);
+			},
 
-		this.areaPath.datum(data).attr('d', this.area);
+			events: {
+				'enter': function() {
+					var chart = this.chart();
 
-		this._onWindowResize();
+					// chart  # refer to this context, used it to access xScale, yScale, width, height, etc. chart property
+					// this   # refer to each individual group just appended by insert command
+					if(chart.onEnter) { chart.onEnter(chart, this); }
 
-		return data;
-	},
+					chart._onWindowResize(chart, this);
 
-	// Update Scale, Box Size, and attr values
-	_update: function(_width, _height) {
+					this.attr({
+						'd': chart.area,
+						'opacity': 1e-6
+					});
+				},
 
-		this.areaPath.attr({
-			'd': this.area
+				'enter:transition': function() {
+					var chart = this.chart();
+					return this
+							.duration(1000)
+							.style('opacity', 1);
+				}
+			}
 		});
 	}
+
+	// 	this.linePath = this.base.append('path').attr({
+	// 		'class': 'line',
+	// 	});
+
+	// 	if(this.onDataBind) { this.onDataBind(); }
+
+	// 	this.linePath.datum(data).attr('d', this.line);
+
+	// 	// Define this fn where the data is manipulated
+	// 	// after all the data var has the correct value, then call it on Window resize
+	// 	// Normally, after calling this fn, need to define the _update to handle the Scale change, box size changes, attr updates
+	// 	this._onWindowResize();
+
+	// 	return data;
+	// }
+
+	// Update Scale, Box Size, and attr values
+	// _update: function(_width, _height) {
+
+	// 	this.linePath.attr({
+	// 		'd': this.line
+	// 	});
+	// }
 });
