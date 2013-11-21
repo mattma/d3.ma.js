bfc4f229f6181fdaa1e9e0ac7b6a5e2f99268920
Matt Ma <mma@adchemy.com>
2013-11-20 17:22:48 -0800

    add the simple Line constrcutor for testing

---
+++ src/basic/simpleLine.js
@@ -0,0 +1,60 @@
+/*
+ */
+d3.chart('Base').extend('SimpleLine', {
+
+	initialize: function(options) {
+		this.options = options = options || {};
+
+		this.simpleLine = this.base.append('svg:line').classed('simple-line', true);
+
+		this.layer('line', this.simpleLine, {
+			dataBind: function(data) {
+				var chart = this.chart();
+
+				if(chart.onDataBind) { chart.onDataBind( data, chart ); }
+
+				// data[options.data]  will return a single array, data will bind path element to each array index,
+				// by pushing options array into an anonymous array, ONLY one path element will be created
+				//return this.selectAll('path').data( (options.data) ? [ data[options.data] ]: data );
+				return this.data( data );
+			},
+
+			insert: function(){
+				var chart = this.chart();
+				if(chart.onInsert) { chart.onInsert(chart); }
+				return chart.simpleLine;
+			},
+
+			events: {
+				'enter': function() {
+					var chart = this.chart();
+
+					// chart  # refer to this context, used it to access xScale, yScale, width, height, etc. chart property
+					// this   # refer to each individual group just appended by insert command
+					if(chart.onEnter) { chart.onEnter(chart, this); }
+				},
+
+				'merge': function() {
+					var chart = this.chart();
+
+					chart._onWindowResize(chart, this);
+				},
+
+				'exit:transition': function() {
+					var chart = this.chart();
+					this
+						.duration(400)
+						.ease('cubic-in')
+						.style( 'opacity', 1e-6)
+						.remove();
+				}
+			}
+		});
+	},
+
+	_update: function( _width, _height, chart, single ) {
+		if(this.update) {
+			this.update( _width, _height, chart, single );
+		}
+	}
+});
