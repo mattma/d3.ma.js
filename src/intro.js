f9aae7fbba3ad4b5f993c634efa0869116bb4f8e
Matt Ma <mma@adchemy.com>
2014-03-23 07:34:59 -0700

    update the support for the wrapper of d3.ma header

--- src/intro.js
+++ src/intro.js
@@ -4,7 +4,9 @@
 	} else if (typeof define === 'function' && define.amd) {
 		// AMD. Register as an anonymous module.
 		define(['d3'], factory);
+	} else {
+		factory( window.d3 );
 	}
-} (this, function (d3) {
+} (typeof window !== "undefined" ? window : this, function (d3) {
 
 'use strict';
