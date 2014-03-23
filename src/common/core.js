48c499b8ee0b0f39904f9917b917841125d5feef
Matt Ma <mma@adchemy.com>
2014-03-23 06:31:17 -0700

    update the core, using the d3chart assert to detect the d3 appearance

--- src/common/core.js
+++ src/common/core.js
@@ -1,15 +1,9 @@
-var d3 = window.d3;
-
 var d3ma = d3.ma = {version: '0.2.0'}; // semver;
 
 d3ma.assert = function(test, message) {
 	if(test) { return; }
 	throw new Error('[d3.ma] ' + message);
 };
-
-d3ma.assert(d3, 'd3.js is required');
-
-d3ma.assert( typeof d3.version === 'string' && d3.version.match(/^3/), 'd3.js version 3 is required' );
 
 
 
