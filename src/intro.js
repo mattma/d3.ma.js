3466aeaa9fd4fe008de777ff71f6ec3f384ceb36
Matt Ma <mma@adchemy.com>
2014-03-23 07:10:59 -0700

    update the intro and outro to handle the factory cases

--- src/intro.js
+++ src/intro.js
@@ -1,4 +1,12 @@
-!function(){
+(function (root, factory) {
+	if (typeof exports === 'object') {
+		module.exports = factory( require('d3') );
+	} else if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(['d3'], factory);
+	}
+} (this, function (d3) {
 
 'use strict';
 
+console.log('fire me');
--- src/outro.js
+++ src/outro.js
@@ -1,8 +1,1 @@
-	if (typeof define === "function" && define.amd) {
-		define(d3ma);
-	} else if (typeof module === "object" && module.exports) {
-		module.exports = d3ma;
-	} else {
-		this.d3ma = d3ma;
-	}
-}();
+}));
