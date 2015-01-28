fb98e9fbd1b822ff6dd672f3b32522a0a0268f27
Matt Ma <bigmabig@gmail.com>
2015-01-27 19:16:33 -0800

    insert each node element into the tree graph

--- src/basic/tree.js
+++ src/basic/tree.js
@@ -19,6 +19,19 @@ d3.chart('Base').extend('Tree', {
         var chart = this.chart();
         if (chart.onDataBind) {
           chart.onDataBind(data, chart);
+
+          function collapse (d) {
+            if (d.children) {
+              d._children = d.children;
+              d._children.forEach(collapse);
+              d.children = null;
+            }
+          }
+
+          data.x0 = options.info.canvasH / 2;
+          data.y0 = 0;
+
+          data.children.forEach(collapse);
         }
         if (chart.onDataTransform) {
           data = chart.onDataTransform(data, chart);
@@ -27,7 +40,16 @@ d3.chart('Base').extend('Tree', {
         chart.nodes = tree.nodes(data).reverse();
         chart.links = tree.links(chart.nodes);
 
-        return this.data(data);
+        // Normalize for fixed-depth.
+        chart.nodes.forEach(function (d) {
+          d.y = d.depth * 180;
+        });
+
+        var i = 0;
+
+        return this.selectAll("g.node").data(chart.nodes, function (d) {
+          return d.id || (d.id = ++i);
+        });
       },
 
       // insert actual element, defined its own attrs
@@ -37,13 +59,24 @@ d3.chart('Base').extend('Tree', {
           chart.onInsert(chart);
         }
 
-        var i = 0;
-        // Update the nodes…
-        chart.node = chart.base.selectAll("g.node")
-          .data(chart.nodes, function (d) {
-            return d.id || (d.id = ++i);
-          });
+        // Enter any new nodes at the parent's previous position.
+        this.append("g")
+          .attr("class", "node");
+          //.attr("transform", function (d) {
+          //  return d;
+          //  //return "translate(" + d.y0 + "," + d.x0 + ")";
+          //});
+        //.on("click", click);
 
+        //nodeEnter.append("circle")
+        //  .attr("r", 1e-6)
+        //  .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+        //
+        //nodeEnter.append("text")
+        //  .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
+        //  .attr("dy", ".35em")
+        //  .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start";
+        // }) .text(function(d) { return d.name; }) .style("fill-opacity", 1e-6);
         return chart.node;
       },
 
@@ -51,6 +84,8 @@ d3.chart('Base').extend('Tree', {
       events:   {
         'enter': function () {
           var chart = this.chart();
+
+          console.log('this: ', this);
 
           // onEnter fn will take two args
           // chart  # refer to this context, used it to access xScale, yScale, width, height, etc.
