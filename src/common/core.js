037b5a4e2aca1fc1c89168bbaa395efcb3dd1c9a
Matt Ma <bigmabig@gmail.com>
2015-10-16 14:38:43 -0700

    clean up the common/core.js and convert it to es6 syntax

--- src/common/core.js
+++ src/common/core.js
@@ -1,4 +1,7 @@
-var d3ma = {version: '0.2.1'}; // semver;
+// semver
+let d3ma = {
+  version: '0.2.1'
+};
 
 // useful to figure out how many svg elements on the current page
 // track individual .canvas element
@@ -8,9 +11,9 @@ d3ma.canvas = [];
 // getter & setter function
 // set the canvas object into the d3ma.canvas array, used to redraw the chart
 // optional param, if omit, it will simply retrieve the current d3ma.canvas status
-d3ma.setCanvas = function(canvas) {
-	if(!canvas && typeof canvas !== 'object') {
-		return d3ma.canvas;
-	}
-	d3ma.canvas.push(canvas);
+d3ma.setCanvas = canvas => {
+  if (!canvas && typeof canvas !== 'object') {
+    return d3ma.canvas;
+  }
+  d3ma.canvas.push(canvas);
 };
