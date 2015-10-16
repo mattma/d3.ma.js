2dd234e23138eb4aa7b4b7d050b531a5aaf8cf38
Matt Ma <bigmabig@gmail.com>
2015-10-16 14:33:26 -0700

    concat the jsfiles in order

--- gulpfile.js
+++ gulpfile.js
@@ -6,36 +6,14 @@ var $ = require('gulp-load-plugins')({
   pattern: ['gulp-*'],
   scope: ['devDependencies']
 });
+var streamqueue  = require('streamqueue');
 
 var jsSrcFiles = [
   'gulpfile.js',
   'src/**/*.js',
   '!src/*.js'
 ];
-
-var jsSrcFilesOrderList = [
-  'src/intro.js',
-  'src/lib/d3chart.js',
-  'src/common/core.js',
-  'src/common/utils.js',
-  'src/common/container.js',
-  'src/components/clippath.js',
-
-  'src/core/scale.js',
-  'src/core/base.js',
-
-  'src/components/axis.js',
-
-  'src/basic/bars.js',
-  'src/basic/line.js',
-  'src/basic/area.js',
-  'src/basic/circle.js',
-  'src/basic/tree.js',
-  'src/basic/simpleLine.js',
-  'src/basic/diagonalLine.js',
-
-  'src/outro.js'
-];
+var destPath = './';
 
 gulp.task('jshint', function () {
   return gulp.src(jsSrcFiles)
@@ -52,69 +30,52 @@ gulp.task('lint', ['jscs', 'jshint']);
 
 // @describe compile es6 modules into amd modules
 gulp.task('buildjs', function () {
-  return gulp.src(jsSrcFilesOrderList)
+  return streamqueue({objectMode: true},
+      gulp.src('src/intro.js'),
+
+      gulp.src('src/lib/d3chart.js'),
+      gulp.src('src/common/core.js'),
+      gulp.src('src/common/utils.js'),
+      gulp.src('src/common/container.js'),
+      gulp.src('src/components/clippath.js'),
+
+      gulp.src('src/core/scale.js'),
+      gulp.src('src/core/base.js'),
+
+      gulp.src('src/components/axis.js'),
+
+      gulp.src('src/basic/bars.js'),
+      gulp.src('src/basic/line.js'),
+      gulp.src('src/basic/area.js'),
+      gulp.src('src/basic/circle.js'),
+      gulp.src('src/basic/tree.js'),
+      gulp.src('src/basic/simpleLine.js'),
+      gulp.src('src/basic/diagonalLine.js'),
+
+      gulp.src('src/outro.js')
+    )
+    .pipe($.concat('d3.ma.js'))
     .pipe($.babel({
       blacklist: ['useStrict']
     }))
-    .pipe($.concat('d3.ma.js'))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest(destPath));
 });
 
 // @describe build out the library
 gulp.task('build', ['buildjs'], function () {
-  gutil.log(
-    gutil.colors.green('[-done:] d3.ma.js and d3.ma.min.js has been generated!')
-  );
+  return gulp.src('d3.ma.js')
+    .pipe($.uglify())
+    .pipe($.concat('d3.ma.min.js'))
+    .on('end', function () {
+      gutil.log(gutil.colors.green(
+        '[-done:] d3.ma.js and d3.ma.min.js has been generated!'));
+    })
+    .pipe(gulp.dest(destPath));
 });
-
-// copy all the core files and release to production
-gulp.task('releaseClient',
-  ['clean','lint', 'build'],
-  function () {
-    var src = 'client/index.html';
-    var dest = 'build/client';
-    var assets = $.useref.assets({searchPath: 'client'});
-
-    // clean task has to be done
-    // imagemin will minify all images and copy into build
-    gulp.start('imagemin');
-
-    return gulp.src(src)
-      .pipe($.replace(
-        /<script src="http:\/\/localhost:\d+\/livereload\.js\?snipver=\d+"><\/script>(\s+)?/g, ''))
-      // handle file concatenation but not minification.
-      // usage: <!-- build:js scripts/combined.js --><!-- endbuild -->
-      .pipe(assets)
-      // Concatenate And Minify JavaScript
-      .pipe($.if('*.js', $.uglify({preserveComments: 'some'})))
-      // Concatenate And Minify Styles
-      .pipe($.if('*.css', $.csso()))
-      .pipe(assets.restore())
-      .pipe($.useref())
-      .pipe($.if('*.html', $.minifyHtml()))
-      .pipe(gulp.dest(dest));
-    // .pipe($.size({title: '[-log:] client folder'}));
-  });
 
 gulp.task('release', ['releaseServer'], function () {
   // reset the development libraries
   gulp.start('envDev');
-  gutil.log(
-    gutil.colors.green('[-done:] Application has been successfully built at'),
-    gutil.colors.magenta('~/build ')
-  );
-  gutil.log(
-    gutil.colors.bold('[-copy:] => cd build '),
-    gutil.colors.gray('# navigate to the freshly built application')
-  );
-  gutil.log(
-    gutil.colors.bold('[-copy:] => npm install '),
-    gutil.colors.gray('# install the dependencies in build folder')
-  );
-  gutil.log(
-    gutil.colors.bold('[-copy:] => node server '),
-    gutil.colors.gray('# running your application in production mode')
-  );
 });
 
 gulp.task('test', ['prepareTests'], function () {
