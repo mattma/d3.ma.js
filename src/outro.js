dfc3d1b4c35a6da685030f2b63009ff597d28df4
Matt Ma <bigmabig@gmail.com>
2015-10-16 22:50:25 -0700

    take care of different occasion on using d3.ma.js like in Node.js, AMD, Common, and Browser

--- src/outro.js
+++ src/outro.js
@@ -1,3 +1,11 @@
-   // when use directly in the browser, export d3.ma as a global
-   return typeof window !== 'undefined' ? d3.ma = d3ma : d3ma;
+  if (typeof define === "function" && define.amd) {
+    // used in amd environment
+    define(d3ma);
+  } else if (typeof module === "object" && module.exports) {
+    // used in Node.js environment
+    module.exports = d3ma;
+  } else {
+    // used in browser environment, export d3.ma as a global
+    return typeof window !== 'undefined' ? d3.ma = d3ma : d3ma;
+  }
 }));
